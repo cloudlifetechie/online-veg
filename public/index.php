2a60c235158a955f31ea497c870368187fb0cd89
gopi musunuri <cloudlifetechie@gmail.com>
2025-03-20 17:35:52 +0000

    php code

--- public/index.php
+++ public/index.php
@@ -1,45 +1,10 @@
 <?php
-require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../controllers/UserController.php';
-require_once __DIR__ . '/../controllers/CartController.php';
-require_once __DIR__ . '/../controllers/OrderController.php';
-require_once __DIR__ . '/../controllers/ProductController.php';
+// Start session
+session_start();
 
-$action = $_GET['action'] ?? 'home';
+// Include configuration file
+require_once '../config/config.php';
 
-switch ($action) {
-    case 'login':
-        $controller = new UserController();
-        $controller->login();
-        break;
-    case 'logout':
-        $controller = new UserController();
-        $controller->logout();
-        break;
-    case 'dashboard':
-        $controller = new UserController();
-        $controller->dashboard();
-        break;
-    case 'products':
-        $controller = new ProductController();
-        $controller->listProducts();
-        break;
-    case 'add_to_cart':
-        $controller = new CartController();
-        $controller->addToCart();
-        break;
-    case 'view_cart':
-        $controller = new CartController();
-        $controller->viewCart();
-        break;
-    case 'place_order':
-        $controller = new OrderController();
-        $controller->placeOrder();
-        break;
-    case 'order_success':
-        include __DIR__ . '/../views/order_success.php';
-        break;
-    default:
-        include __DIR__ . '/../views/home.php';
-        break;
-}
+// Load the homepage
+require_once '../views/home.php';
+?>
