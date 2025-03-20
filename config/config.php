de98470d930f8456e249fd52fedd3dc05d159dab
gopi musunuri <cloudlifetechie@gmail.com>
2025-03-20 13:59:16 +0000

    php code

--- config/config.php
+++ config/config.php
@@ -6,8 +6,8 @@ define('DB_PASS', '');
 define('DB_NAME', 'vegetable_shop');
 
 // Stripe configuration
-define('STRIPE_SECRET_KEY', 'sk_test_XXXXXXXXXXXXXXXXXXXXXXXX'); // Replace with your Stripe Secret Key
-define('STRIPE_PUBLIC_KEY', 'pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'); // Replace with your Stripe Publishable Key
+define('STRIPE_SECRET_KEY', 'sk_test_XXXXXXXXXXXXXXXXXXXXXXXX'); 
+define('STRIPE_PUBLIC_KEY', 'pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'); 
 
 // Base URL
 define('BASE_URL', 'http://localhost/vegetable-shop/public/');
